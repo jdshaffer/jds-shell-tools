d53e548a10d3342e661f350399eae3ceb9183567
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2026-01-26 14:00:12 +0900

    Enhance send_ip.sh for machine-specific IP retrieval
    Updated the script to handle different machine types and improved console feedback.

--- jds-programs/send_ip.sh
+++ jds-programs/send_ip.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
-# -----------------------------------------------------------------
+#-----------------------------------------------------------------
 # Universal send_ip.sh
-# Jeffrey D. Shaffer
-# Updated -- 2025-12-24
+# 2026-01-26
 #
-# A little program that automatically sends the local machine's
-# name (set in .bash_aliases) and ip address to a local
-# machine set as an IP hub. This allows for the automated
-# lookup of IP addresses, even with local IPs change over time
-# (reboots, etc.)
-#
-# -----------------------------------------------------------------
-# Notes:
-#    - Requires ifconfig to be installed
-#      (might need to run:  sudo apt install net-tools
+# 2025-06-27
+#    - Current version will exit gracefully if the remote machine
+#      does not repond (i.e., is not found) after 5 seconds
+#    - Be sure to update "machine_name" in .bash_aliases
 #    - Be sure to update and check the "Configuration" below
 #
-# -----------------------------------------------------------------
+# 2025-07-04
+#    - Changed the curl command to hide the server's response
+#
+# 2026-01-26
+#    - Now checks which machine is being used before looking
+#      up the local IP. One of my machines (raspi) needs
+#      special treatment because of a Docker container that
+#      reports a different, conflicting IP
+#    - Tidied up the console feedback provided by the problem.
+#
+#-----------------------------------------------------------------
+
 
 # Configuration
-RPI_HUB_IP="192.168.1.1"    # Be sure to set this to your IP-hub machine's IP!
+RPI_HUB_IP="192.168.3.71"
 RPI_HUB_PORT="5000"
 DEVICE_HOSTNAME=$machine_name
 CURL_TIMEOUT=5    # How long to wait before exiting gracefully
 
 
-# Get the primary IP address of the current device
-DEVICE_IP=""
-DEVICE_IP=$(/sbin/ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
+# Check the machine name, then get the primary IP address for the current device
+case "$machine_name" in
+    raspi)
+        # wlan0 is added to avoid multiple IPs, such as those caused by a running Docker container
+        DEVICE_IP=""
+        DEVICE_IP=$(/sbin/ifconfig wlan0 | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
+        ;;
+    *)
+        # This is MacOS
+        DEVICE_IP=""
+        DEVICE_IP=$(/sbin/ifconfig | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
+        ;;
+esac
+
 
 # Check if IP was successfully retrieved
 if [ -z "$DEVICE_IP" ]; then
@@ -36,12 +51,12 @@ if [ -z "$DEVICE_IP" ]; then
 fi
 
 
-# Construct the JSON payload
+# Construct the JSON payload to send to the IP-Hub
 JSON_PAYLOAD="{\"hostname\": \"${DEVICE_HOSTNAME}\", \"ip_address\": \"${DEVICE_IP}\"}"
 
 
 # Send the report using curl with a timeout (for graceful exit on network errors)
-echo "Reporting IP ${DEVICE_IP} for ${DEVICE_HOSTNAME} to http://${RPI_HUB_IP}:${RPI_HUB_PORT}/report_ip"
+echo "Reporting IP ${DEVICE_IP} for this computer (${DEVICE_HOSTNAME}) to the IP-Hub..."
 curl -s --connect-timeout ${CURL_TIMEOUT} --max-time ${CURL_TIMEOUT} -X POST \
      -H "Content-Type: application/json" \
      -d "${JSON_PAYLOAD}" \
@@ -54,10 +69,14 @@ CURL_EXIT_STATUS=$?
 
 if [ ${CURL_EXIT_STATUS} -eq 0 ]; then
     echo "IP report sent successfully."
+    echo
 elif [ ${CURL_EXIT_STATUS} -eq 28 ]; then
     echo "Error: IP report failed due to timeout. The IP Hub might be unreachable or unresponsive."
+    echo
     exit 1 # Exit with an error code for timeout
 else
     echo "Error: Failed to send IP report. Curl exited with status ${CURL_EXIT_STATUS}."
+    echo
     exit 1 # Exit with an error code for other curl errors
 fi
+
