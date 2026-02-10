b7750b5ca31f9b046f759f3a9d329f45d4e25647
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2026-02-11 08:44:41 +0900

    Refactor youtube-stuff.sh comments and clean up code
    Updated script comments and removed unused functions.

--- jds-scripts/youtube-stuff.sh
+++ jds-scripts/youtube-stuff.sh
@@ -1,14 +1,14 @@
 # ----------------------------------------------------------------------------------
-# Download Related Bash Scripts
+# YouTube Related Bash Scripts
 # Jeffrey D. Shaffer
-# Updated -- 2026-02-08
+# Updated -- 2026-02-11
 #
 # Notes:
 #    - These functions require the program yt-dlp
 #    - yt-dlp can be installed via the terminal
 #          brew install yt-dlp   # MacOS
 #          apt  install yt-dlp   # Linux
-#    - ytmusic depents on the program mpv
+#    - ytmusic depents on the program mpv 
 #          brew install mpv      # MacOS
 #
 # ----------------------------------------------------------------------------------
@@ -34,7 +34,7 @@ downloadmp4(){      # Download the given YouTube video as an MP4 file
 
 ytmusic() {
     set +m   # disable job completion messages
-
+    
     local timer=""
     local duration=0
     local url
@@ -78,7 +78,7 @@ ytmusic() {
 
     # --- run mpv as normal ---
     mpv --no-video "$url"
-
+    
     echo
     }
 
@@ -97,43 +97,5 @@ web2txt(){          # Save the given URL as a TXT file
     cd ${HOME}/Downloads
     lynx -dump "$1" > web2txt-output.txt
     open web2txt-output.txt
-    echo " "
-    }
-
-
-grabtrans(){       # Download YouTube video transcript to a file called "transcript.txt"
-    echo " "
-    cd ${HOME}/Downloads
-    echo "Downloading YouTube Transcript..."
-    yt-dlp --skip-download --write-subs --write-auto-subs --sub-lang en --sub-format ttml --convert-subs srt --output "transcript.%(ext)s" "$1" && sed -i '' -e '/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]$/d' -e '/^[[:digit:]]\{1,4\}$/d' -e 's/<[^>]*>//g' ./transcript.en.srt && sed -e 's/<[^>]*>//g' -e '/^[[:space:]]*$/d' transcript.en.srt > transcript.txt && rm transcript.en.srt
-    open transcript.txt
-    echo " "
-    }
-
-
-summary(){         # Summarize the contents of a YouTube video using the transcripts and Ollama
-    echo " "
-    cd ${HOME}/Downloads
-    echo "Downloading YouTube Transcript..."
-    yt-dlp --skip-download --write-subs --write-auto-subs --sub-lang en --sub-format ttml --convert-subs srt --output "transcript.%(ext)s" "$1" && sed -i '' -e '/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]$/d' -e '/^[[:digit:]]\{1,4\}$/d' -e 's/<[^>]*>//g' ./transcript.en.srt && sed -e 's/<[^>]*>//g' -e '/^[[:space:]]*$/d' transcript.en.srt > transcript.txt && rm transcript.en.srt
-    echo " "
-    echo "Sharing the transcript with Ollama..."
-    ollama run gemma3:4b "Can you summarize this please?" < transcript.txt > summary.txt
-    rm transcript.txt
-	more summary.txt
-    echo " "
-    }
-
-
-say-summ(){         # Have the terminal read the video summary aloud
-    echo " "
-    cd ${HOME}/Downloads
-    echo "Downloading YouTube Transcript..."
-    yt-dlp --skip-download --write-subs --write-auto-subs --sub-lang en --sub-format ttml --convert-subs srt --output "transcript.%(ext)s" "$1" && sed -i '' -e '/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]$/d' -e '/^[[:digit:]]\{1,4\}$/d' -e 's/<[^>]*>//g' ./transcript.en.srt && sed -e 's/<[^>]*>//g' -e '/^[[:space:]]*$/d' transcript.en.srt > transcript.txt && rm transcript.en.srt
-    echo " "
-    echo "Sharing the transcript with Ollama..."
-    ollama run gemma3:4b "Can you summarize this please?" < transcript.txt > summary.txt
-    rm transcript.txt
-    say < summary.txt
     echo " "
     }
